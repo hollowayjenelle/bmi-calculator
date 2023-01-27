8ec37c5c862a2527d60ce3c37e34667c126ce6f9
jholloway <jholloway@griddynamics.com>
2023-01-27 12:29:34 -0500

    add BMI message to display area

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -18,31 +18,48 @@ const Calculator: FC = () => {
         setHeight(Number(event.target.value))
     }
 
+    function changeMessage(){
+        let text = ''
+        if(BMI < 18.5){
+            text= 'underweight'
+        }else if(BMI >= 18.5 && BMI < 25){
+            text='healthy'
+        }else if (BMI >25.0 && BMI < 30){
+            text='overweight'
+        }else{
+            text='obese'
+        }
+        return `You are ${text}`
+    }
+
     return (
-        <div>
-            <h4>Weight: {weight} lbs</h4>
-            <input 
-                type="range" 
-                name="weight" 
-                value={weight}
-                onChange={changeWeight}
-                min={1}
-                max={1000}
-                step={1} >
-            </input>
-            <h4>Height: {height} inches</h4>
-            <input 
-                type="range" 
-                name="height" 
-                value={height}
-                onChange={changeHeight}
-                min={1}
-                max={1000}
-                step={1} >
-            </input>
-            <div>
+        <div className='calculation-area'>
+            <div className='input-area'>
+                <h4>Weight: {weight} lbs</h4>
+                <input 
+                    type="range" 
+                    name="weight" 
+                    value={weight}
+                    onChange={changeWeight}
+                    min={1}
+                    max={1000}
+                    step={1} >
+                </input>
+                <h4>Height: {height} inches</h4>
+                <input 
+                    type="range" 
+                    name="height" 
+                    value={height}
+                    onChange={changeHeight}
+                    min={1}
+                    max={1000}
+                    step={1} >
+                </input>
+            </div>
+            <div className='display-area'>
                 <h4>Your BMI is:</h4>
                 {BMI > 0 && <p>{BMI}</p>}
+                {BMI > 0 && <h4>{changeMessage()}</h4>}
             </div>
         </div>
     );
